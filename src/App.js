e85facd357f0c50f55e6c5bd0c4162d38ec4a5c2
15797vishal <115143001+15797vishal@users.noreply.github.com>
2023-02-12 00:05:18 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+ import React,{useState} from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   const [num1, setNum1]=useState("Enter the number 1");
+   const [num2, setNum2]=useState("Enter the number 2");
+   const [ans, setans]=useState(0);
+
+   const handlechange= (event)=>{
+
+    const {name,value}=event.target;
+    
+     if(name==="num1")setNum1(parseInt(value));
+     else if(name==="num2") setNum2(parseInt(value));
+    console.log(event);
+   }  
+
+   const handleaction =(action) =>{ 
+    if(action =="+" ) setans(num1+num2)
+    else if(action ==="-")setans(num1-num2)
+    else if(action ==="/") setans(num1/num2)
+    else if(action==="*")setans(num1*num2);
+    else{
+      console.log("error please enter valid response")
+    } 
+    if(action.length<0){
+      window.alert("number cannot be empty ")
+    }
+   }; 
+
+  return ( 
+    
+    <div className="App">  
+    
+    <h2>react calulator </h2>
+<form>
+   
+    <input type="number" name="num1" value={num1} 
+    onChange={handlechange}  placeholder="Enter the number 1" /><br></br>
+    <input type="number" name="num2" value={num2}
+     onChange={handlechange}  placeholder="Enter the number 2"/><br></br>
+    
+    <button type="button"  onClick= {() => handleaction("+")}>+</button>
+    <button  type="button" onClick={() => handleaction("-")}>-</button> 
+    <button  type="button" onClick={() => handleaction("/")}>/</button> 
+    <button  type="button" onClick={() => handleaction("*")}>*</button> 
+    <h3>ans: {ans}</h3>
+</form>
+
+    </div> 
+  
+
   );
 }
 
